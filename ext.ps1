900d01b193f7931a7b979293361a60bfe61d8493
vijishankar <67778907+vijishankar@users.noreply.github.com>
2021-04-26 18:07:45 +0530

    Update ext.ps1

--- ext.ps1
+++ ext.ps1
@@ -1,3 +1,8 @@
+Function Delete-RG
+{
 
+$pipelinename 
 
-bat "curl -X GET http://http://54.174.181.118:8080/job/${params.projectname}/3/api/json --user Vengatesh:Vengatesh@1"
+bat "curl -X GET http://http://54.174.181.118:8080/job/$pipelinename/3/api/json --user DevopsUser:CptDevops@111"
+
+}
