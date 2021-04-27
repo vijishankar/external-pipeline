05eb383790a0a7417c775263ab4ef852706d3223
vijishankar <67778907+vijishankar@users.noreply.github.com>
2021-04-27 18:36:50 +0530

    Update ext.ps1

--- ext.ps1
+++ ext.ps1
@@ -7,7 +7,7 @@ $pipelinename = "external-pipeline"
 Invoke-WebRequest uri ' curl -X GET http://54.174.181.118:8080/job/azresource-creation/lastSuccessfulBuild/api/json  -UseDefaultCredentials'
 
 
- //sh 'curl -X GET http://54.174.181.118:8080/job/azresource-creation/lastSuccessfulBuild/api/json --user DevopsUser:CptDevops@111'
+ 
 
 
  
