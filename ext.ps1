b0ebcd5c92642803f30be4043ab903f2345f7cc5
vijishankar <67778907+vijishankar@users.noreply.github.com>
2021-04-27 19:13:34 +0530

    Update ext.ps1

--- ext.ps1
+++ ext.ps1
@@ -6,8 +6,10 @@ $pipelinename = "external-pipeline"
   
 
 
-Invoke-WebRequest -Uri 'http://54.174.181.118:8080/job/azresource-creation/lastSuccessfulBuild/api/json   -UseDefaultCredentials'
- 
+$uri = "http://54.174.181.118:8080/job/Paysaas_sampath/lastSuccessfulBuild/api/json"
+$wc = New-Object System.Net.WebClient
+$wc.UseDefaultCredentials = $true
+$json = $wc.DownloadString($uri)
 
 
  
