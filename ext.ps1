0a75548f821dece7e0dcaba8956e847a08edce53
vijishankar <67778907+vijishankar@users.noreply.github.com>
2021-04-27 19:45:33 +0530

    Update ext.ps1

--- ext.ps1
+++ ext.ps1
@@ -6,9 +6,7 @@ $pipelinename = "external-pipeline"
   
 
 
-$userpassB64 = [byte[]][char[]]"DevopsUser:CptDevops@111" | ConvertTo-Base64 -NoLineBreak
-Write-Output ("Credentials: " + $userpassB64)
-$res = Invoke-WebRequest http://54.174.181.118:8080/job/azresource-creation/lastSuccessfulBuild/api/json -Headers @{Authorization = "Basic $userpassB64"}
+az rest --method get --url http://54.174.181.118:8080/job/azresource-creation/lastSuccessfulBuild/api/json 
       
 
 
