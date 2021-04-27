46cdd905fee41074896baaa5b3de7a22a74964f3
vijishankar <67778907+vijishankar@users.noreply.github.com>
2021-04-27 10:04:48 +0530

    Update ext.ps1

--- ext.ps1
+++ ext.ps1
@@ -5,4 +5,6 @@ $pipelinename = "external-pipeline"
 
 sh 'curl -X GET http://54.174.181.118:8080/job/azresource-creation/83/api/json --user DevopsUser:CptDevops@111'
 
+println System.getenv("ENVIRONMENT") 
+
 
