0786a082f7cb8453b39e864a7e9edc678e54a98a
vijishankar <67778907+vijishankar@users.noreply.github.com>
2021-04-27 09:40:56 +0530

    Update ext.ps1

--- ext.ps1
+++ ext.ps1
@@ -3,6 +3,6 @@
 
 $pipelinename = "external-pipeline"
 
-bat "curl -X GET http://http://54.174.181.118:8080/job/${pipelinename}/3/api/json --user DevopsUser:CptDevops@111"
+bat "curl -X GET http://http://54.174.181.118:8080/job/"${pipelinename}"/3/api/json --user DevopsUser:CptDevops@111"
 
 
