edc6e62a6deaf8f3652455d53225d56760e84dbd
vijishankar <67778907+vijishankar@users.noreply.github.com>
2021-04-27 10:10:30 +0530

    Update ext.ps1

--- ext.ps1
+++ ext.ps1
@@ -6,6 +6,6 @@ $pipelinename = "external-pipeline"
 sh 'curl -X GET http://54.174.181.118:8080/job/azresource-creation/83/api/json --user DevopsUser:CptDevops@111'
 
  $Env = build.buildVariableResolver.resolve("ENVIRONMENT") 
- Wriite-Host $Env
+ Write-Host $Env
 
 
