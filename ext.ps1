f6f23f14356f6b44f3f11a3287407beb30cc079c
vijishankar <67778907+vijishankar@users.noreply.github.com>
2021-04-27 12:03:55 +0530

    Update ext.ps1

--- ext.ps1
+++ ext.ps1
@@ -3,7 +3,9 @@
 
 $pipelinename = "external-pipeline"
 
-sh 'curl -X GET http://54.174.181.118:8080/job/azresource-creation/latest/api/json --user DevopsUser:CptDevops@111'
+def buildNumber = Jenkins.instance.getItem('azresource-creation').lastSuccessfulBuild.number
+
+sh 'curl -X GET http://54.174.181.118:8080/job/azresource-creation/buildNumber/api/json --user DevopsUser:CptDevops@111'
 
  build.buildVariableResolver.resolve("ENVIRONMENT") 
  
