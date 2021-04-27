841c2096a45e01dde2aea8d7716f66d001fa4f2b
vijishankar <67778907+vijishankar@users.noreply.github.com>
2021-04-27 18:40:08 +0530

    Update ext.ps1

--- ext.ps1
+++ ext.ps1
@@ -4,9 +4,20 @@
 $pipelinename = "external-pipeline"
 
   
-Invoke-WebRequest uri ' curl -X GET http://54.174.181.118:8080/job/azresource-creation/lastSuccessfulBuild/api/json  -UseDefaultCredentials'
 
+$user = 'DevopsUser'
+$pass = 'CptDevops@111'
 
+$pair = "$($user):$($pass)"
+
+$encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($pair))
+
+$basicAuthValue = "Basic $encodedCreds"
+
+$Headers = @{
+    Authorization = $basicAuthValue
+}
+Invoke-WebRequest -Uri 'http://54.174.181.118:8080/job/azresource-creation/lastSuccessfulBuild/api/json' -Headers $Headers
  
 
 
