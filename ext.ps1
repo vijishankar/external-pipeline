4d697a7455f458b22de46f2a869f2389e3366465
vijishankar <67778907+vijishankar@users.noreply.github.com>
2021-04-28 11:14:15 +0530

    Update ext.ps1

--- ext.ps1
+++ ext.ps1
@@ -8,7 +8,8 @@ $pipelinename = "external-pipeline"
 
 
 
- sh "curl -D- -u DevopsUser:CptDevops@111 -X GET -H 'Content-Type: application/json' http://54.174.181.118:8080/job/azresource-creation/lastSuccessfulBuild/api/json"
+ $value = sh "curl -D- -u DevopsUser:CptDevops@111 -X GET -H 'Content-Type: application/json' http://54.174.181.118:8080/job/azresource-creation/lastSuccessfulBuild/api/json"
+  Write-Host "${value}"
 
       
 
