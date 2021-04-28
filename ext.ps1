f16cc78798246953460933ab783f4f7eaabe4635
vijishankar <67778907+vijishankar@users.noreply.github.com>
2021-04-28 10:30:01 +0530

    Update ext.ps1

--- ext.ps1
+++ ext.ps1
@@ -3,10 +3,7 @@
 
 $pipelinename = "external-pipeline"
 
-  
-
-
-powershell 'sh \'curl -X GET http://54.174.181.118:8080/job/azresource-creation/lastSuccessfulBuild/api/json --user DevopsUser:CptDevops@111\''
+ 'sh \'curl -X GET http://54.174.181.118:8080/job/azresource-creation/lastSuccessfulBuild/api/json --user DevopsUser:CptDevops@111\''
       
 
 
